9d5a0ac3f4ce6704ff8a1f8aa1c0f6409e913e76
Brent B <brentbaughan1@gmail.com>
2021-05-19 17:15:11 -0400

    created schema; continued adding server info

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const hbs = require('express-handlebars');
-const routes = require('./controllers');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const expresshandlebars = require("express-handlebars");
+const routes = require("./controllers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const handlebars = expresshandlebars.create();
+
+const sess = {
+  secret: "the entire unix philosophy",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+});
