7ea46b1c95edc22a05eb008575b8e9b8f9b8a147
Brent B <brentbaughan1@gmail.com>
2021-05-26 16:48:52 -0400

    created Comment model and added relationships to index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,34 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
+User.hasmany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
-module.exports = { User, Post }
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = { User, Post, Comment }
