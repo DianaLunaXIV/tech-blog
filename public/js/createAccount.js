70abbbad298e213458df1da619431af2c81c075f
Brent B <brentbaughan1@gmail.com>
2021-08-05 20:36:54 -0400

    created script to allow user creation

---
+++ public/js/createAccount.js
@@ -0,0 +1,33 @@
+async function postUserData(data)
+{
+    let fetchResult = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    console.log(data)
+    console.log(fetchResult)
+    return fetchResult;
+}
+
+async function onCreateUser(event) {
+    event.preventDefault();
+    let userData = {
+        "name": document.getElementById('name-create').value.trim(),
+        "email": document.getElementById('email-create').value.trim(),
+        "password": document.getElementById('password-create').value.trim()
+    }
+
+    let createResults = await postUserData(userData);
+
+    if (createResults.ok) {
+        alert(`User created for ${userData.email}. Welcome, ${userData.name}! Please login.`)
+    } else {
+        alert(`Failed to create user data.`)
+        return;
+    }
+}
+
+document.querySelector('.create-account-button').addEventListener('submit', onCreateUser);
