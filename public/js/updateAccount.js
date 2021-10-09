7cac109c8c995a3bd97a828ec7f90d83f589f758
Brent <brentbaughan1@gmail.com>
2021-10-09 00:12:08 -0400

    created updateUser.js and added fields to profile.handlebars

---
+++ public/js/updateAccount.js
@@ -0,0 +1,34 @@
+async function putUserData(data) {
+    let fetchResult = await fetch('/api/users', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    console.log(data)
+    console.log(fetchResult)
+    return fetchResult;
+}
+
+async function onUpdateUser(event) {
+    event.preventDefault();
+
+    let userData = {
+        "currentPassword": document.getElementById("profile-password-current").value.trim(),
+        "newPassword:": document.getElementById("profile-password-change").value.trim(),
+        "confirmPassword": document.getElementById("profile-password-confirm").value.trim()
+    }
+
+    let putResults = await putUserData(userData);
+
+    if (putResults.ok) {
+        alert(`Your password has been updated.`)
+        return
+    } else {
+        alert(`Password reset failed. Please try again.`)
+        return
+    }
+}
+
+document.querySelector('.profile-manage-button').addEventListener('click', onUpdateUser);
